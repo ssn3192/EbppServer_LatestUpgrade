863797546bc8d0261abbae13d7baaa2b79228464
Shivani Narang <shivu.narang@gmail.com>
2016-10-17 13:45:02 -0500

    done done....!! Yay

--- EbppApp/App/controllers/invoiceController.js
+++ EbppApp/App/controllers/invoiceController.js
@@ -17,7 +17,6 @@ app.controller('invoiceController', function (
     transactionHistoryFactory,
     paymentFactory,
     achFactory,
-    achHistoryFactory,
     bankFactory) {
     var vm = this;
 
